f4f51b8e49e56712902b78ca6f3533601002bcee
Lukas de Souza Santos <lk@serrante.com.br>
2023-09-27 12:43:11 -0300

    fix: voltando

--- src/index.js
+++ src/index.js
@@ -25,13 +25,15 @@ const GlobalStyle = createGlobalStyle`
 
 `;
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
