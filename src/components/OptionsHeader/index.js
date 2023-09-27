63c08bbd344b6b7b15e49d418b70d1bde084e4ed
Lukas de Souza Santos <lk@serrante.com.br>
2023-09-27 18:26:57 -0300

    feat: :zap: adição de uma ancora e hover para as opções do header

--- src/components/OptionsHeader/index.js
+++ src/components/OptionsHeader/index.js
@@ -16,6 +16,20 @@ const Option = styled.li`
     min-width: 120px;
 `;
 
+const Link = styled.a`
+    text-decoration: none;
+    color: inherit;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    width: 100%;
+    height: 150%;
+    
+    &:hover {
+        background-color: #f0f0f0; /* Adapte a cor conforme necessário */
+      }
+`;
+
 
 const textOptions = ['CURRICULO', 'SOBRE MIM', 'PROJETOS', 'CONTATO']
 
@@ -23,7 +37,11 @@ export default function OptionsHeader() {
     return (
         <Options key='1'>
             {textOptions.map((text) => (
-                <Option key={text}><p>{text}</p></Option>
+                <Option key={text}>
+                    <Link href={`#${text.toLowerCase().trim()}`}>
+                        <span>{text}</span>
+                    </Link>
+                </Option>
             ))}
         </Options>
     )
